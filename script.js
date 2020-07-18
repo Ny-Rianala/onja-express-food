f7dea4c9927c3862021cc8bd35c0f275ef47be6d
kati <fanomezantsoakatiuscia048@gmail.com>
2020-07-18 16:29:17 +0300

    adding html in script.js file

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const addOrder = document.querySelector('.add-order');
 const outerModal = document.querySelector('.outer-modal');
 const innerModal = document.querySelector('.inner-modal');
+const submitButton = document.querySelector(".submitOrder");
+const orderedList = document.querySelector(".order-list");
 
 const openModal = e => {
     outerModal.classList.add('open');
+    
+    const card = event.target.closest('.order-list');
+	const { dish, size, amount} = card.dataset;
+	const myHTML = `
+            <img src="${img}"/>
+            <p>Order : ${dish} ${size} ${amount}</p>
+    `;
+	orderedList.innerHTML = myHTML;
 };
 
-const handleClick = e => {
-    console.log('remove class');
 
+const handleClick = e => {
     const isOutside = !e.target.closest('.inner-modal');
     if (isOutside) {
         outerModal.classList.remove('open');
@@ -21,6 +30,15 @@ const handleEscape = e => {
     }
 };
 
+
 window.addEventListener('keydown', handleEscape);
 outerModal.addEventListener('click', handleClick);
 addOrder.addEventListener('click', openModal);
+submitButton.addEventListener("click", orderedList);
+
+//event delegation
+const handleModalButton = event => {
+	if (event.target.matches('button.submitOrder')) {
+	}
+};
+window.addEventListener('click', handleModalButton);
