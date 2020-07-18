8c8a5d6d76419225e986a14d2bf53480a179eac3
kati <fanomezantsoakatiuscia048@gmail.com>
2020-07-18 12:00:10 +0300

    adding the adding order modal

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('good luck!');
+const addOrder = document.querySelector('.add-order');
+const outerModal = document.querySelector('.outer-modal');
+const innerModal = document.querySelector('.inner-modal');
+
+const openModal = e => {
+    outerModal.classList.add('open');
+};
+
+const handleClick = e => {
+    console.log('remove class');
+
+    const isOutside = !e.target.closest('.inner-modal');
+    if (isOutside) {
+        outerModal.classList.remove('open');
+    }
+};
+
+const handleEscape = e => {
+    if (e.key === 'Escape') {
+        outerModal.classList.remove('open');
+    }
+};
+
+window.addEventListener('keydown', handleEscape);
+outerModal.addEventListener('click', handleClick);
+addOrder.addEventListener('click', openModal);
