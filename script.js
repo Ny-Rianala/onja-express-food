eb9a6f19de138583a1e8017d5a8895fd58ec5ec5
kati <fanomezantsoakatiuscia048@gmail.com>
2020-07-20 10:30:48 +0300

    changing some of the code

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const addOrder = document.querySelector('.add-order');
 const outerModal = document.querySelector('.outer-modal');
 const innerModal = document.querySelector('.inner-modal');
-const orderedList = document.querySelector(".order");
+const list = document.querySelector(".order");
 
 //Open modal to show the form to the user
 const openModal = e => {
     outerModal.classList.add('open');
 };
 
-const openListModal = e => {
+const orderedList = event => {
     outerModal.classList.add("openDetail");
     const listDetail = event.target.closest('.order');
     const title = listDetail.querySelector('span').textContent;
@@ -17,7 +17,6 @@ const openListModal = e => {
     //Put it in modal
     const myHTML = `
         <span>${title}</span>
-        <img src="${img}"/>
         <p>Order : ${dish} ${size} ${amount}</p>
     `;
     innerModal.innerHTML = myHTML;
@@ -25,13 +24,14 @@ const openListModal = e => {
 
 //Adding eventListener to listen for click
 addOrder.addEventListener('click', openModal);
+list.addEventListener("click", orderedList);
 
 //event delegation
-window.addEventListener('click', (e) => {
+window.addEventListener('click', (event) => {
 	if (event.target.matches('button.submitOrder')) {
-        event.target(".addOrder");
+        event.target(".order");
     }
     if(event.target.matches("button.details")){
-        return;
+        return listDetail;
     }
 });
